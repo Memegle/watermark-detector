be58cf1263ca11e88b81c202d443a60dd18ad0f2
Xuewei Yan <x1yan@ucsd.edu>
2021-01-03 12:52:36 -0800

    starter-code v1.0

---
+++ RetinaNet.py
@@ -0,0 +1,36 @@
+class RetinaNet():
+
+    def __init__(self, dataset, num_images=200, pretrained=None):
+
+        # TODO
+
+    """
+    Load data from dataset on google drive (finish train-test split in this method)
+    Split into images and labels
+    num_images = number of images per label (e.g. 200 positive and 200 negative)
+    """
+    def __load_data(self, dataset, num_images=200):
+
+        # TODO
+
+        return ...
+    
+    """
+    Helper method for training
+    """
+    def __resize_image(self, height=200, width=200):
+
+        # TODO
+
+        return ...
+
+    ### DEFINE METHODS FOR TRAINING THE NET
+
+    """
+    Report the accuracy of classification
+    """
+    def evaluate(self, images, labels):
+
+        # TODO
+
+        return ...
---
+++ WatermarkDetectron.py
@@ -0,0 +1,78 @@
+class WatermarkDetectron:
+
+    """
+    Constructor of the WatermarkDetectron
+    @param pretrained: The version number of a pretrained model. If none, load 
+                       the latest version. Raise error if no model is found.
+    """
+    def __init__(self, pretrained=None):
+        
+        # TODO
+
+        self.model = self.__load_model(pretrained)
+
+    """
+    Load the model trained by RetinaNet.py from /data/model_saved
+    @param None
+    @return The model loaded
+    """
+    def __load_model(self, pretrained=None):
+
+        # TODO
+
+        return ...
+    
+    """
+    Load an image from an URL address
+    @param url: URL address of the image
+    @return A generator of this image 
+    """
+    def __read_file_from_url(self, url):
+
+        # TODO
+        
+        return ...
+
+    """
+    Load an image from a local path
+    @param filepath: the local path of the image(s)
+    @return A generator of the image(s)
+    """
+    def __read_file_from_local_path(self, filepath):
+        # Consider two cases:
+        # 1. The filepath refers directly to an image file.
+        #    In this case, make a generator with only this image.
+        # 2. The filepath refers to a folder containing images.
+        #    In this case, make a generator with all the valid images
+        #    in that folder (jpg, png, jpeg, etc.).
+        
+        # TODO
+
+        return ...
+
+    
+    """
+    Callable method that identifies the filepath type (url or local), then
+    use the helper methods to generate the expected generator.
+    @param filepath: The path of the image file(s)
+    @return A generator of the image(s)
+    """
+    def load_image(self, filepath):
+        
+        # TODO
+
+        return ...
+    
+    """
+    Use RetinaNet.predict() to predict the position of watermarks (if any),
+    then transform that into binary outputs (0=no watermark, 1=has watermark).
+    @param: The generator with images
+    @return List of predictions (FOR NOW)
+    """
+    def predict(self, images):
+
+        # TODO
+
+        return ...
+    
+    ### REMAINS TO DO: connecting to our db
