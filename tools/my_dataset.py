d445652e042a071b5b56abbf6d55d8d366f105a5
Jessica <zhz386@ucsd.edu>
2021-01-03 16:14:17 -0800

    change data generator

--- tools/my_dataset.py
+++ tools/my_dataset.py
@@ -1,17 +1,18 @@
 import numpy as np
-import torch
-import os
-import random
-from PIL import Image
-from torch.utils.data import Dataset
+import keras
 
-class MyDataset(Dataset):
-    def __init__(self, data_dir, mode="train"):
-
-    def __getitem__(self, index):
-
-        return img, label
+class DataGenerator(keras.utils.Sequence):
+    'Generates data for Keras'
+    def __init__(self, list_IDs, labels, batch_size=32, dim=(32,32,32), n_channels=1,
+                 n_classes=10, shuffle=True):
+        'Initialization'
 
     def __len__(self):
-
+        'Denotes the number of batches per epoch'
         return len
+
+    def __getitem__(self, index):
+        'Generate one batch of data
+
+        return img, label
+## reference: https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
