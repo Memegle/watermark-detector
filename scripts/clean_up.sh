20491d5c91f83b62eeaa9364bf0fb3b328172c8f
Xuewei Yan <50162542+XueweiYan@users.noreply.github.com>
2021-04-30 01:32:04 -0700

    pytorch-version (#18)
    Co-authored-by: XueweiYan <xueweiyan0405@gmail.com>

--- scripts/clean_up.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 -m pip install --upgrade pip || ((count++))
-python3 -m pip install tensorflow || ((count++))
-python3 -m pip install keras || ((count++))
--- scripts/train.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python3 scripts/generate_XML.py
-cd retinanet_keras
-python3 VOCdevkit/VOC2007/voc2retinanet.py
-python3 voc_annotation.py
-python3 train.py
