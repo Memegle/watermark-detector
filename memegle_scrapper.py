effef8062084d86772534773b60f2d92145cd9e6
tonyh4156 <tonyhu4156@gmail.com>
2021-05-17 13:43:11 -0700

    add indexing

--- memegle_scrapper.py
+++ memegle_scrapper.py
@@ -5,11 +5,13 @@ from os import mkdir
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--output_dir', default='./internal-data/')
+parser.add_argument('-i', '--start_index', default='0')
 args = parser.parse_args()
 
 DOWNLOAD_FOLDER = abspath(args.output_dir)
 URL = "https://memegle.live:8080/all"
 TIMES_RUN = 0
+START_INDEX = int(args.start_index)
 success = 0
 
 if not isdir(DOWNLOAD_FOLDER):
@@ -18,15 +20,18 @@ if not isdir(DOWNLOAD_FOLDER):
 
 with urllib.request.urlopen(URL) as url:
     data = json.loads(url.read().decode())
-    for img in data:
-    	try: 
-    		TIMES_RUN += 1
-    		img_url = img['media_url']
-    		filename = img['title'] + '.' + img['ext']
-    		path = join(DOWNLOAD_FOLDER, filename)
-    		urllib.request.urlretrieve(img_url, path)
-    		success += 1
-    	except Exception as e:
-    		print('failing at {} with error: {}'.format(img['media_url'], e))
+    for i in range(START_INDEX, len(data)):
+        img = data[i]
+        try: 
+            TIMES_RUN += 1
+            img_url = img['media_url']
+            filename = img['title'] + '.' + img['ext']
+            path = join(DOWNLOAD_FOLDER, filename)
+            urllib.request.urlretrieve(img_url, path)
+            success += 1
+            print("Success: " + filename)
+            print("Finished Index: " + str(i))
+        except Exception as e:
+            print('failing at {} with error: {}'.format(img['media_url'], e))
 
 print('successfully downloaded {} images out of {}'.format(success, TIMES_RUN-success))
