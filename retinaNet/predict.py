041137c35692614f71faa6f8ca20b7149451a508
Xuewei Yan <50162542+XueweiYan@users.noreply.github.com>
2021-08-03 03:19:24 -0700

    still need args in predictor (#29)
    Co-authored-by: XueweiYan <xueweiyan0405@gmail.com>

--- retinaNet/predict.py
+++ retinaNet/predict.py
@@ -27,6 +27,45 @@ def time_synchronized():
     torch.cuda.synchronize() if torch.cuda.is_available() else None
     return time.time()
 
+def load_model(device):
+    model = create_model(num_classes=1)
+    # load train weights
+    train_weights = "./save_weights/model.pth"
+    assert os.path.exists(train_weights), "{} file dose not exist.".format(train_weights)
+    model.load_state_dict(torch.load(train_weights, map_location=device)["model"])
+    model.to(device)
+    return model
+
+def single_image_classifier(img_path):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("using {} device.".format(device))
+    model = load_model(device)
+    original_img = Image.open(img_path)
+    if img_path[-4:] == '.gif': # If the input image is a gif, pick the first frame for testing
+        original_img.seek(0)
+    # from pil image to tensor, do not normalize image
+    data_transform = transforms.Compose([transforms.ToTensor()])
+    img = data_transform(original_img)
+    # expand batch dimension
+    img = torch.unsqueeze(img, dim=0)
+
+    model.eval()  # 进入验证模式
+    with torch.no_grad():
+        # init
+        img_height, img_width = img.shape[-2:]
+        init_img = torch.zeros((1, 3, img_height, img_width), device=device)
+        model(init_img)
+        t_start = time_synchronized()
+        predictions = model(img.to(device))[0]
+        t_end = time_synchronized()
+        print("inference+NMS time: {}".format(t_end - t_start))
+        predict_boxes = predictions["boxes"].to("cpu").numpy()
+        predict_classes = predictions["labels"].to("cpu").numpy()
+        predict_scores = predictions["scores"].to("cpu").numpy()
+        if len(predict_boxes) == 0 or max(predict_scores) < 0.5:  # Can change this value for trade-off between AP and Recall
+            return False, -1.0
+        else:
+            return True, max(predict_scores)
 
 def main():
     # get devices
@@ -43,13 +82,6 @@ def main():
     model.load_state_dict(torch.load(train_weights, map_location=device)["model"])
     model.to(device)
 
-    # read class_indict
-    label_json_path = './pascal_voc_classes.json'
-    assert os.path.exists(label_json_path), "json file {} dose not exist.".format(label_json_path)
-    json_file = open(label_json_path, 'r')
-    class_dict = json.load(json_file)
-    category_index = {v: k for k, v in class_dict.items()}
-
     prediction_file = open("../data/predictions.txt", 'w')
     folder_path = '../data/test_images/'
     entries = os.listdir(folder_path)
@@ -57,7 +89,8 @@ def main():
         img_path = folder_path + img_name
         # load image
         original_img = Image.open(img_path)
-
+        if img_path[-4:] == '.gif': # If the input image is a gif, pick the first frame for testing
+            original_img.seek(0)
         # from pil image to tensor, do not normalize image
         data_transform = transforms.Compose([transforms.ToTensor()])
         img = data_transform(original_img)
@@ -80,10 +113,10 @@ def main():
             predict_classes = predictions["labels"].to("cpu").numpy()
             predict_scores = predictions["scores"].to("cpu").numpy()
 
-            if len(predict_boxes) == 0 or max(predict_scores) < 0.6:  # Can change this value for trade-off between AP and Recall
-                prediction_file.write(img_name + ", " + str(False) + "\n")
+            if len(predict_boxes) == 0 or max(predict_scores) < 0.5:  # Can change this value for trade-off between AP and Recall
+                prediction_file.write("{}, {}, {}\n".format(img_name, "False", -1.0))
             else:
-                prediction_file.write(img_name + ", " + str(True) + "\n")
+                prediction_file.write("{}, {}, {}\n".format(img_name, "True", max(predict_scores)))
                 
     prediction_file.close()
 
